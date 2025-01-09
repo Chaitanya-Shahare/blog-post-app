72d9535f075402d080477ce97922fb4ad4c1e166
Chaitanya Shahare <chaitanyashahare@Chaitanyas-MacBook-Air.local>
2025-01-09 11:25:20 +0530

    Add Not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    <div className="p-2 sm:p-4">
+      <h1 className="text-4xl font-bold">Not Found</h1>
+      <p className="text-lg font-bold">
+        The page you are looking for does not exist. Please check the URL and
+        try again.
+      </p>
+    </div>
+  );
+}
